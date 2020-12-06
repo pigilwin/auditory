fcd5ca3cdd8cf3299d5fb614e8fdc27495b81e2b
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-12-06 22:17:27 +0000

    Adding panels

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,9 +1,13 @@
-function Main() {
-    return (
-        <div id="main-panel" className="container mx-auto flex flex-wrap overflow-hidden currently-visible-panel">
+import { ControlPanel } from "./controls/panel"
+import { DrumPanel } from "./drums/panel"
+import { NotesPanel } from "./notes/panel"
 
+export const Main = (): JSX.Element => {
+    return (
+        <div id="main-panel" className="container mx-auto flex flex-wrap overflow-hidden">
+            <NotesPanel/>
+            <DrumPanel/>
+            <ControlPanel/>
         </div>
     );
 }
-
-export default Main;
