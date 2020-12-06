bb2c0751543dd876d275b71df5df336a3ea4de4d
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-12-06 22:17:21 +0000

    Adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { AboutPanel } from './about';
 import {NavBar} from './components/NavBar';
+import {Main} from './Main';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar/>
-    </div>
+      <Switch>
+        <Route path="/">
+          <Main/>
+        </Route>
+        <Route path="/about">
+          <AboutPanel/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
