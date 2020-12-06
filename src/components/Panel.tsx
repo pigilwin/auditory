cdaa24ee853d54d5626403953523143ea0095f1e
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-12-06 22:16:24 +0000

    Panel component now uses children

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,12 +1,12 @@
-export function Panel({result}: {result: JSX.Element}) {
+export const Panel = (props: React.PropsWithChildren<{}>): JSX.Element => {
     return (
         <div className="w-1/3 px-4 overflow-hidden">
-            {result}
+            {props.children}
         </div>
     );
 }
 
-export function PanelTitle({title}: {title: string}) {
+export const PanelTitle = ({title}: {title: string}): JSX.Element => {
     return (
         <h1 className="text-center text-xl p-4">{title}</h1>
     );
