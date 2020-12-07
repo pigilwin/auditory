705286062f71497593b3edbae5fa5b9d6fb9d0fe
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-12-07 11:36:55 +0000

    Removing bloc

--- src/track/event.ts
+++
@@ -1,7 +0,0 @@
-export abstract class AbstractTrackEvent {
-
-}
-
-export class VolumeChangeEvent extends TrackEvent {
-
-}
--- src/track/state.ts
+++
@@ -1,7 +0,0 @@
-export abstract class AbstractTrackState {
-    
-}
-
-export class EmptyTrackState extends AbstractTrackState {
-
-}
