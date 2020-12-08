037892eb5a651c90b8edcadd15a73958871d62db
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-12-08 14:23:40 +0000

    Adding begin page

--- src/builder/begin.tsx
+++ src/builder/begin.tsx
@@ -1,7 +1,42 @@
+import { Button } from "../components/Buttons"
+import { TextSingleLineInput } from "../components/Inputs";
+import { PanelTitle } from "../components/Panel"
+
 export const Begin = (): JSX.Element => {
+
+    const createNewTrackClickHandler = (): void => {
+
+    };
+
+    const loadExistingTracksClickHandler = (): void => {
+
+    };
+
+    const saveTrackNameHandler = (): void => {
+
+    };
+
     return (
         <div className="container mx-auto flex flex-wrap overflow-hidden">
-            
+            <div className="w-1/2 flex flex-col">
+                <div className="w-full p-5">
+                    <PanelTitle title="Create New"/>
+                </div>
+                <div className="w-full p-5">
+                    <TextSingleLineInput id="track-name" title="Track Name" onKeyUp={saveTrackNameHandler}/>
+                </div>
+                <div className="w-full text-center p-5">
+                    <Button id="create-new-track" title="Create New Track" onClick={createNewTrackClickHandler}/>
+                </div>
+            </div>
+            <div className="w-1/2 flex flex-col">
+                <div className="w-full p-5">
+                    <PanelTitle title="Load Existing"/>
+                </div>
+                <div className="w-full text-center">
+                    <Button id="load-tracks" title="Load Tracks" onClick={loadExistingTracksClickHandler}/>
+                </div>
+            </div>
         </div>
     );
 };
