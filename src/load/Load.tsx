17a25df1c618041cf31c8660594ef406b626e747
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-12-09 15:24:44 +0000

    Began work on load page

--- src/load/Load.tsx
+++ src/load/Load.tsx
@@ -1,3 +1,11 @@
+import { Title } from "../builder/Title"
+
 export const LoadPanel = (): JSX.Element => {
-    return (<div></div>);
+    return (
+        <div className="container mx-auto flex flex-wrap overflow-hidden">
+            <div className="text-center w-full">
+                <Title title="Load Tracks"/>
+            </div>
+        </div>
+    );
 }
