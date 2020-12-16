5a355cf5de4822c4544b2d11ed12b441a9b80038
Tim Remnant <timwebdesign2013@gmail.com>
2020-12-16 18:26:36 +0000

    Converting panel to bottom bar

--- src/builder/controls/panel.tsx
+++ src/builder/controls/panel.tsx
@@ -1,11 +1,10 @@
-import { Panel, PanelTitle } from "../../components/Panel";
 import { RangeInput, ToggleSwitch } from "../../components/Inputs";
-import { Button } from "../../components/Buttons";
 import { SavedTrack } from "../../store/track/trackTypes";
 import { useDispatch, useSelector } from "react-redux";
 import { updateVolumeAsync, updatePannerAsync, updateLoopingAsync, addLayerAsync } from "../../store/track/trackEvent";
 import { isPlayingSelector } from "../../store/track/trackSlice";
 import { Audio } from "../../audio/audio";
+import { Play, Stop } from "../../components/icon";
 
 interface ControlPanelState {
     track: SavedTrack;
@@ -29,10 +28,6 @@ export const ControlPanel = ({track}: ControlPanelState): JSX.Element => {
         dispatch(updateLoopingAsync(event.currentTarget.checked, track.id));
     };
 
-    const addLayer = (): void => {
-        dispatch(addLayerAsync(track.id));
-    };
-
     const startTrack = async (): Promise<void> => {
         await Audio.playTrack(track);
     };
@@ -42,48 +37,54 @@ export const ControlPanel = ({track}: ControlPanelState): JSX.Element => {
     };
 
     return (
-        <Panel>
-            <PanelTitle title="Controls"/>
-            <div className="grid grid-cols-1">
-                <RangeInput 
-                    id="volume"
-                    min={0}
-                    max={10}
-                    value={track.control.volume}
-                    step={1}
-                    title="Volume"
-                    onChange={volumeChange}
-                    error=""
-                />
-                <RangeInput
-                    id="panner"
-                    min={-1}
-                    max={1}
-                    value={track.control.panner}
-                    step={1}
-                    title="Left & Right"
-                    onChange={panner}
-                    error=""
-                />
-                <ToggleSwitch
-                    id="looping"
-                    title="Looping"
-                    value={track.control.looping}
-                    onChange={looping}
-                    error=""
-                />
+        <section id="bottom-navigation" className="block fixed inset-x-0 bottom-0 z-10 bg-white shadow">
+            <div id="tabs" className="flex justify-between">
+                <div className="w-full text-center">
+                    <RangeInput 
+                        id="volume"
+                        min={0}
+                        max={10}
+                        value={track.control.volume}
+                        step={1}
+                        title="Volume"
+                        onChange={volumeChange}
+                        error=""
+                    />
+                </div>
+                <div className="w-full justify-center inline-block text-center cursor-pointer pt-2 pb-1">
+                    <button onClick={startTrack}>
+                        <Play/>
+                        <span className="tab tab-kategori block text-xs">Play</span>
+                    </button>
+                </div>
+                <div className="w-full justify-center inline-block text-center pt-2 pb-1">
+                    <ToggleSwitch
+                        id="looping"
+                        title="Looping"
+                        value={track.control.looping}
+                        onChange={looping}
+                        error=""
+                    />
+                </div>
+                <div className="w-full justify-center inline-block text-center cursor-pointer pt-2 pb-1">
+                    <button onClick={stopTrack}>
+                        <Stop/>
+                        <span className="tab tab-whishlist block text-xs">Stop</span>
+                    </button>
+                </div>
+                <div className="w-full justify-center inline-block text-center">
+                    <RangeInput
+                        id="panner"
+                        min={-1}
+                        max={1}
+                        value={track.control.panner}
+                        step={1}
+                        title="Left & Right"
+                        onChange={panner}
+                        error=""
+                    />
+                </div>
             </div>
-            <div className="grid grid-cols-3">
-                <div className="p-2 text-center">
-                    <Button disabled={isPlaying} title="Start" onClick={startTrack}/>
-                </div>
-                <div className="p-2 text-center">
-                    <Button disabled={false} title="Add Layer" onClick={addLayer}/>
-                </div>
-                <div className="p-2 text-center">
-                    <Button disabled={!isPlaying} title="Stop" onClick={stopTrack}/>
-                </div>
-            </div>
-        </Panel>
+	    </section>
     );
 }
