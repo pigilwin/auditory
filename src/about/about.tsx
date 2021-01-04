a982ff37b51f2463d671c3984851afd727be75f5
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-04 00:10:16 +0000

    Removing text center

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -2,7 +2,7 @@ export const AboutPanel = (): JSX.Element => {
     return (
         <div className="px-4 overflow-hidden">
             <h1 className="text-6xl text-center">About</h1>
-            <ul className="mx-auto text-center">
+            <ul className="mx-auto p-2">
                 <li className="text-2xl">Sounds is a sound generation and track building application built using the Web Audio Api.</li>
                 <li className="text-2xl">Why did i build this? Good question.</li>
                 <li className="text-2xl">This app allowed me to use the Web Audio Api and modern Javascript features to build a new application.</li>
