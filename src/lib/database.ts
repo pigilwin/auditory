cadd4b1781fbc368c55962145f97353a6fe8c74d
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-11 22:18:59 +0000

    Adding configuration object store

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,17 +1,23 @@
 import { DBSchema, IDBPDatabase, openDB } from 'idb';
 import { SavedTrack } from '../store/track/trackTypes';
+import { Configuration } from './configuration';
 
 export const openDatabase = async (): Promise<IDBPDatabase<TrackDatabase>> => {
     return await openDB<TrackDatabase>('tracks', 1, {
         upgrade: (db) => {
             db.createObjectStore('tracks');
+            db.createObjectStore('configuration');
         }
-    }); 
+    });
 }
 
 interface TrackDatabase extends DBSchema {
     tracks: {
         key: string;
         value: SavedTrack;
+    },
+    configuration: {
+        key: string;
+        value: Configuration;
     }
 }
