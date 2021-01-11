34bcfc3b3b7773847ba8580486f5eb419f94f708
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-11 22:40:01 +0000

    Adding full background colors

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,6 +1,6 @@
 export const AboutPanel = (): JSX.Element => {
     return (
-        <div className="px-4 overflow-hidden">
+        <div className="px-4 overflow-hidden min-h-screen">
             <h1 className="text-6xl text-center">About</h1>
             <ul className="mx-auto p-2">
                 <li className="text-2xl">Sounds is a sound generation and track building application built using the Web Audio Api.</li>
--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -2,7 +2,7 @@ import { Title } from '../components/Title';
 
 export const Settings = (): JSX.Element => {
     return (
-        <div className="container mx-auto flex flex-wrap overflow-hidden">
+        <div className="mx-auto flex flex-wrap overflow-hidden min-h-screen">
             <div className="text-center w-full">
                 <Title title="Settings"/>
             </div>
