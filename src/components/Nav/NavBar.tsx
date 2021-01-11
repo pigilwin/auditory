9ae754569b372376046c51d82b56e8d112e4adbd
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-11 00:00:57 +0000

    Adding theme colours

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -7,7 +7,7 @@ export const NavBar = (): JSX.Element => {
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="lg:flex-grow text-center lg:text-left">
-                    <h1 className="text-3xl">Sounds</h1>
+                    <h1 className="text-3xl dark:text-white">Sounds</h1>
                 </div>
                 <GoBack/>
                 <div className="text-sm lg:flex-grow text-center lg:text-right">
@@ -31,7 +31,7 @@ interface NavButtonProps {
 
 const NavButton = ({id, title, to}: NavButtonProps): JSX.Element => {
     return (
-        <Link to={to} id={id} className="block mt-4 lg:inline-block lg:mt-0 text-black cursor-pointer hover:text-blue-100 mr-4 text-lg">
+        <Link to={to} id={id} className="block mt-4 lg:inline-block lg:mt-0 text-black dark:text-white cursor-pointer hover:text-blue-400 dark:hover:text-blue-400 mr-4 text-lg">
             {title}
         </Link>
     );
