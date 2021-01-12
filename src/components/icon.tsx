06afff67bb866d8594101e5fb587de2cd25473f2
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-12 21:15:18 +0000

    Icon reacts to theme change

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,6 +1,6 @@
 export const Play = (): JSX.Element => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="inline-block mb-1" fill="black" width="25" height="25">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="inline-block mb-1 fill-current text-black dark:text-white" width="25" height="25">
             <path d="M0 0h24v24H0z" fill="none"/>
             <path d="M8 5v14l11-7z"/>
         </svg>
@@ -9,7 +9,7 @@ export const Play = (): JSX.Element => {
 
 export const Stop = (): JSX.Element => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="inline-block mb-1" fill="black" width="25" height="25">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="inline-block mb-1 fill-current text-black dark:text-white" width="25" height="25">
             <path d="M0 0h24v24H0z" fill="none"/>
             <path d="M6 6h12v12H6z"/>
         </svg>
