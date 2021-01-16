ece1052215ce9a00a87534f97efdbf5755cfd3c2
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-16 22:09:06 +0000

    Adding padding, simple text generation

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,11 +1,20 @@
+const text: string[] = [
+    "Sounds is a sound generation and track building application built using the Web Audio Api.",
+    "Why did i build this? Good question.",
+    "This app allowed me to use the Web Audio Api and modern Javascript features to build a new application."
+];
+
 export const AboutPanel = (): JSX.Element => {
+    
+    const elements: JSX.Element[] = text.map((v) => {
+        return <li className="text-2xl p-4">{v}</li>;
+    });
+    
     return (
         <div className="px-4 overflow-hidden min-h-screen">
             <h1 className="text-6xl text-center">About</h1>
             <ul className="mx-auto p-2">
-                <li className="text-2xl">Sounds is a sound generation and track building application built using the Web Audio Api.</li>
-                <li className="text-2xl">Why did i build this? Good question.</li>
-                <li className="text-2xl">This app allowed me to use the Web Audio Api and modern Javascript features to build a new application.</li>
+                {elements}
             </ul>
         </div>
     );
